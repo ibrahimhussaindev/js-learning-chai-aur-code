5a3ed52c39730213b7a0fd0bf080a0b4fc271876
ibrahimhussaindev <ibrahimhussain.dev@gmail.com>
2025-05-23 03:47:24 +0500

    Learned about JavaScript data types and typeof operator
    - Explained primitive and reference data types
    - Used typeof to check data types of different values
    - Added comments to clarify concepts like null, undefined, and strict mode
    - Included console outputs for demonstration

---
+++ basics/dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("Hello World!"); // show alert box
+// we are using node js not browser
+
+console.log("Hello World!"); // print to console
+
+let name = "John Doe"; // string
+let age = 19; // number
+let isLoggedIn = false // boolean
+
+console.log(typeof name); // string
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+
+// primitive data types:
+// number => 2 to power of 53
+// bigint
+// string => "" or ''
+// boolean => true or false
+// null => empty value
+// undefined => variable is declared but not assigned
+// symbol => unique value
+
+// reference data types(non-primitive data types):
+// arrays => list of values
+// objects => key value pairs
+// functions => block of code
